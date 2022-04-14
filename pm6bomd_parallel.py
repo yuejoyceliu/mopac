33c1ac290f29b8726d68a21d2de78ce84c51f01a
yuejoyceliu <45061262+yuejoyceliu@users.noreply.github.com>
2022-04-13 17:08:39 -0700

    Remove usage of using input for extra parameters

--- pm6bomd_parallel.py
+++ pm6bomd_parallel.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
 '''
-#AUTHOR: Yue Liu
-#EMAIL: yueliu96@uw.edu
-#Created: 11/27/2018
-#Edited: 04/08/2019
- Usage:  python pm6bomd_parallel.py (charge, multiplicity and temperature are taken as input later)
-    or:  python pm6bomd_parallel.py charge multiplicity  T (chg & mp are intergers; T is float)
+ AUTHOR: Yue Liu
+ EMAIL: yueliu96@uw.edu
+ Created: 11/27/2018
+ Edited: 04/13/2022
+ Usage:   python pm6bomd_parallel.py charge multiplicity  T (chg & mp are intergers; T is float)
 '''
 import glob,os,sys
-from subprocess import Popen,PIPE
 
 nCORES = 28
 YAML = 'anneal.yaml'
@@ -19,63 +17,30 @@ KEYWORDS = [
 #'trajectory_selection: "1-65"\n'
 ]
 
-if 1/2==0:
-    myinput = raw_input
-else:
-    myinput = input
 
 def checkcommand():
     n = len(glob.glob('*.xyz'))
     if n == 0:
         raise SystemExit(':::>_<:::No xyz Files Found!')
-    if len(sys.argv)==1:
-        return issamecondition(n)
-    elif len(sys.argv)==4:
+    if len(sys.argv)==4:
         try:
             return int(sys.argv[1]),int(sys.argv[2]),float(sys.argv[3])
         except BaseException as err:
             print(':::>_<::: charge and multiplicity must be integers and temperature is float!')
             raise SystemExit(err)
     else:
-        raise SystemExit('Usage: python pm6bomd_parallel.py\nOR     python pm6bomd_parallel.py charge(int) multiplicity(int) temperature(float)')
+        raise SystemExit('Usage: python pm6bomd_parallel.py charge(int) multiplicity(int) temperature(float)')
 
 def mypathexist(dirs):
     for d in dirs:
         if os.path.exists(d):
             raise SystemExit(':::>_<:::%s Exists! Remove it and try again!' % d)
 
-def issamecondition(n):
-    x1,x2,x3 = 'could','be','anything'
-    print('%d xyz files found in the current directory'  % n)
-    print('-'*60+'\nPlease Specify their charge,multiplicity and temperature(K)\n'+'-'*60)
-    try:
-        c1 = myinput('? Do they have the same charge [y/Y--yes],[other--no]: ')
-        if c1.lower()=='y':
-            x1=int(myinput('? charge: '))
-        c2 = myinput('? Do they have the same multiplicity [y/Y--yes],[other--no]: ')
-        if c2.lower()=='y':
-            x2=int(myinput('? multiplicity: '))
-        c3 = myinput('? Do they have the same temperature: [y/Y--yes],[other--no]: ')
-        if c3.lower()=='y':
-            x3=float(myinput('? temperature(K): '))
-        return x1,x2,x3
-    except:
-        raise SystemExit(':::>_<:::Invalid Input!')
-
 def yaml(fl,outfl,chg,mp,t):
     p1 = ['job: dynamics\n','geometry: '+fl+'\n']
     p2 = ['interface: mopac\n','method: pm6\n','mopac_precise: yes\n','mopac_peptide_bond_fix: yes\n','modifiers: dispersion3, h_bonds4\n']
     p3 = ['modifier_h_bonds:\n','  h_bonds4_scale_charged: no\n','  h_bonds4_extra_scaling: {}\n','maxcycles: 20000\n','timestep: 0.001\n']
     p4 = ['thermostat: berendsen\nthermostat_tc: 0.05\n']
-    try:
-        if isinstance(chg,str):
-            chg = int(myinput(('? charge of %s: ') % fl))
-        if isinstance(mp,str):
-            mp = int(myinput(('? multiplicity of %s: ') % fl))
-        if isinstance(t,str):
-            t = float(myinput(('? temperature(K) of %s: ') %fl))
-    except:
-        raise SystemExit(':::>_<:::Invalid Input!')
     with open(outfl,'w') as fo:
         fo.writelines(p1)
         fo.write('charge: '+str(chg)+'\n')                                   
@@ -119,7 +84,6 @@ def parallelrun_sh(n,user,mydir):
 
 def dynamics(charge,mtplct,temp):
     xyzfiles = glob.glob('*.xyz')
-    xyzfiles.sort()
     xyzdirs = ['d'+d[:-4] for d in xyzfiles]
     mypathexist(xyzdirs)
     nxyz = len(xyzfiles)
@@ -130,12 +94,12 @@ def dynamics(charge,mtplct,temp):
         yamlname = dxyz+'/'+YAML
         yaml(fxyz,yamlname,charge,mtplct,temp)
         os.rename(fxyz,dxyz+'/'+fxyz)
-    pwd = Popen('pwd',stdout=PIPE,shell=True).stdout.read().strip().decode()
-    who = Popen('whoami',stdout=PIPE,shell=True).stdout.read().strip().decode()
+    pwd = os.getcwd() 
+    who = os.getlogin()
     tasklists_sh(xyzdirs,pwd)
     parallelrun_sh(nxyz,who,pwd)
     print('**\(^O^)/**You are ready to run mopac dynamics! Check and Run:\n sbatch parallel_run.sh')
-    if KEYWORDS: print(KEYWORDS)
+    if KEYWORDS: print("Additional keywords are added:\n", " ".join(KEYWORDS))
 
 if __name__=='__main__':
     chg,mp,t = checkcommand()
